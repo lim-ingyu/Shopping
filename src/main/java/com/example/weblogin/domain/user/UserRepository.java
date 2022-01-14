bcaca0d0baa024e8a77bb2db8c22e0109b6d7dbc
rladuswl <khkkgon18@naver.com>
2022-01-14 22:03:06 +0900

    프론트엔드 작업 추가

--- src/main/java/com/example/weblogin/domain/user/UserRepository.java
+++ src/main/java/com/example/weblogin/domain/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.weblogin.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// <Entity, Entity-id>
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 }
