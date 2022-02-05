e1359dd5f6a26e89eb6641c50f4c5c53dd2770a9
rladuswl <khkkgon18@naver.com>
2022-02-05 20:54:22 +0900

    완성

--- src/main/java/com/example/weblogin/controller/UserPageController.java
+++ src/main/java/com/example/weblogin/controller/UserPageController.java
@@ -343,4 +343,28 @@ public class UserPageController {
         }
     }
 
+    // 잔액 충전 페이지
+    @Transactional
+    @GetMapping("/user/cash/{id}")
+    public String charge(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        // 로그인 유저와 잔액 충전 페이지에 접속하는 id가 같아야 함.
+        if(principalDetails.getUser().getId() == id){
+            // 유저의 주문내역
+            User user = userPageService.findUser(id);
+            model.addAttribute("user",user);
+
+            return "/user/cash";
+        }else{
+            return "redirect:/main";
+        }
+    }
+
+    // 잔액충전 처리
+    @GetMapping("/user/charge/pro")
+    public String chargePro(int amount, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        User user = userPageService.findUser(principalDetails.getUser().getId());
+        userPageService.chargePoint(user.getId(),amount);
+        return "redirect:/main";
+    }
+
 }
--- src/main/java/com/example/weblogin/service/UserPageService.java
+++ src/main/java/com/example/weblogin/service/UserPageService.java
@@ -26,4 +26,11 @@ public class UserPageService {
         userRepository.save(update);
     }
 
+    public void chargePoint(int id,int amount){
+        User user = userRepository.findById(id);
+        user.setCoin(user.getCoin() + amount);
+
+        userRepository.save(user);
+    }
+
 }
